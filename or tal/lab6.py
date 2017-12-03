9b08d4471cf31486a7103b1745bec27884fe483d
Malak18-Meet <malak.jabarin@gmail.com>
2017-12-03 19:20:44 +0200

    hello

---
+++ or tal/lab6.py
@@ -0,0 +1,22 @@
+from turtle import *
+import random
+
+class Ball(Turtle):
+	def __init__(self, radius, color, speed):
+		Turtle.__init__(self)
+		self.shape("circle")
+		self.shapesize(radius)
+		self.color(color)
+		self.speed(speed)
+
+
+ball1=Ball(3,"blue",12)
+ball2=Ball(2, "red", 8)
+
+# def check_collision(ball1, ball2):
+
+
+
+
+
+mainloop()
